eae3a6eb7495d39e5b043511c1637daad4087ee7
ajay <ajaychagam@gmail.com>
2018-07-19 10:25:26 +0530

    added menu item.

--- src/app/angular-tut/topics/angular-element/angular-element.component.ts
+++ src/app/angular-tut/topics/angular-element/angular-element.component.ts
@@ -8,11 +8,49 @@ import { Title } from '@angular/platform-browser';
 })
 export class AngularElementComponent implements OnInit {
 
+  polyfill: string;
+  componentElm: string;
+  componentDeclar: string;
   constructor(private title: Title) {
     this.title.setTitle('how to create angular elements');
   }
 
   ngOnInit() {
+    this.polyfill = `
+    import '@webcomponents/custom-elements/src/native-shim';
+    import '@webcomponents/custom-elements/custom-elements.min';`;
+
+    this.componentElm = `import { Component, Input } from '@angular/core';
+    @Component({
+        selector: 'app-hello',
+        template:  <span>Angular Elements-{{wish}}</span>,
+    })
+    export class HelloComponent {
+        title = 'Angular Elements';
+        @Input() wish: string;
+    };`;
+
+    this.componentDeclar = `
+ import { BrowserModule } from '@angular/platform-browser';
+ import { NgModule } from '@angular/core';
+ import { MessageComponent } from './message.component';
+
+ @NgModule({
+     declarations: [
+         MessageComponent
+     ],
+     imports: [
+         BrowserModule
+     ],
+     providers: [],
+     bootstrap: [],
+     entryComponents: [HelloComponent]
+ })
+ export class AppModule {
+
+ }`;
   }
+
+
 
 }
